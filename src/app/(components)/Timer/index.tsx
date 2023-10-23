e89dd8f4a199e11b5f800ff3684a6a5181a9e9c9
Caique Morales <caiquemorales20@gmail.com>
2023-10-23 18:48:18 -0300

    :tada: Base content

---
+++ src/app/(components)/Timer/index.tsx
@@ -0,0 +1,9 @@
+// Functional Component
+export default function Timer() {
+  // Rendering
+  return (
+    <div>
+      <h1 className="text-center text-[32px] font-bold text-[#D7E0FF]">Pomo Time</h1>
+    </div>
+  );
+}
