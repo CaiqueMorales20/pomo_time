88cea1b7cb1cced3016013b7936a76352e4be0f4
Caique Morales <caiquemorales20@gmail.com>
2023-10-24 18:46:53 -0300

    :sparkles: feat: Basic timer working

--- src/app/(components)/Timer/index.tsx
+++ src/app/(components)/Timer/index.tsx
@@ -1,9 +1,42 @@
+// Client Side
+'use client'
+
+// Imports
+import { useEffect, useState } from "react";
+
 // Functional Component
 export default function Timer() {
+  // Variables
+  const [running, setRunning] = useState(false)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(40)
+  const [seconds, setSeconds] = useState(0)
+
+  // Functions
+  function decrease() {
+    if (seconds === 0) {
+      setSeconds(59)
+      setMinutes(prev => prev - 1)
+      return
+    }
+    setSeconds(prev => prev - 1)
+  }
+
+  useEffect(() => {
+    if(running) {
+      const interval = setInterval(() => {
+        decrease()
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, );
+
+
   // Rendering
   return (
-    <div>
-      <h1 className="text-center text-[32px] font-bold text-[#D7E0FF]">Pomo Time</h1>
-    </div>
+    <section className="mx-auto flex flex-col gap-[19px] items-center justify-center mt-40">
+      <h2 className="text-[100px] font-bold text-center text-[#D7E0FF]">{minutes.toString().length === 1 ? `0${minutes}` : minutes}:{seconds.toString().length === 1 ? `0${seconds}` : seconds}</h2>
+      <button className="tracking-[15px] text-[#D7E0FF] hover:text-[#F87070] uppercase duration-300" onClick={() => setRunning(!running)}>{running ? 'stop' : 'start'}</button>
+    </section> 
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Timer from './(components)/Timer'
 export default function Home() {
   return (
     <main className='pt-[48px]'>
+      <h1 className="text-center text-[32px] font-bold text-[#D7E0FF]">Pomo Time</h1>
       <Timer />
     </main>
   )
